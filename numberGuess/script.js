7eb32ff2e65ea0487d7589692a7c1030f4b749d8
anjosdev <viniciusanjosdev@gmail.com>
2022-10-07 17:08:28 -0300

    Change numberGuess paragraph and header colors

--- numberGuess/script.js
+++ numberGuess/script.js
@@ -3,4 +3,33 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+  return Math.floor(Math.random()*10)
+}
 
+function compareGuesses(userGuess, computerGuess, secretTarget) {
+  if (userGuess < 0 || userGuess > 9) {
+    alert('Your number is out of range!');
+  }
+  userTargetDifference = Math.abs(secretTarget - userGuess);
+  computerTargetDifference = Math.abs(secretTarget - computerGuess);
+
+  if (userTargetDifference <= computerTargetDifference) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateScore(winner) {
+  switch (winner) {
+    case 'human':
+      humanScore++;
+    case 'computer':
+      computerScore++;
+  }
+}
+
+function advanceRound() {
+  currentRoundNumber++;
+}
